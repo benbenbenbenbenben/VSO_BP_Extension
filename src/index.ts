1ae2f6a8a7f026705b043270538c2260a31c1b8a
Benjamin Babik <work@benjaminbabik.com>
2018-07-02 15:35:02 +0100

    update

--- src/index.ts
+++ src/index.ts
@@ -11,74 +11,85 @@ import StatusIndicator = require("VSS/Controls/StatusIndicator")
 import { ExtensionDataService } from "VSS/SDK/Services/ExtensionData"
 import Utils_String = require("VSS/Utils/String")
 
-// get config
-const getConfig = async () => {
-    const defaultBaseUrl = "https://graph.dcdc.io/extensions/VSO_BP_Extension"
-    const defaultconfig = {
-        baseUrl: defaultBaseUrl,
-        repositoryId: null,
-        repositoryPath: null,
-        repositoryType: null
-    }
-    const service = await VSS.getService<ExtensionDataService>(VSS.ServiceIds.ExtensionData)
-    const savedconfig = await service.getValue("global.config")
-    return { ...defaultconfig, ...savedconfig }
-}
+export class BusinessProcess {
 
-const main = async () => {
-    const config = await getConfig()
-    const projectId = VSS.getWebContext().project.id
-    const projectName = VSS.getWebContext().project.name
-    const gitclient = GitHttpClient.getClient()
-    const tfclient = TfvcRestClient.getClient()
-    const gitRepos = await gitclient.getRepositories(projectId)
-
-    if (config.repositoryType == null) {
-
-        const dlg = $("<div />")
-        dlg.append(`<h3>Project: ${projectName}`)
-        dlg.append(`<p>Where are your business process models stored?</p>`)
-
-        const gitSelect = {
-            enabled: gitRepos.length > 0,
-            mode: "drop",
-            source: gitRepos.map(r => r.name),
-            width: "400px"
-        } as Combos.IComboOptions
-
-        const repType = {
-            mode: "drop",
-            source: [
-                "TFS",
-                "git"
-            ],
-            value: gitRepos.length > 0 ? "git" : "TFS",
-            width: "400px",
-            change() {
-                gitSelectCtrl.setEnabled(this.getText() === "git")
-            }
-        } as Combos.IComboOptions
-
-        $("<label />").text("Repository Type:").appendTo(dlg);
-        const repTypeCtrl = Controls.create(Combos.Combo, dlg, repType)
-        $("<label />").text("Git Repository:").appendTo(dlg);
-        const gitSelectCtrl = Controls.create(Combos.Combo, dlg, gitSelect)
-
-        const dialog = Dialogs.show(Dialogs.ModalDialog, {
-            content: dlg,
-            title: "Configure",
-            okCallback(result: any) {
-                return
-            }
-        } as Dialogs.IModalDialogOptions)
-        const ele = dialog.getElement()
-        ele.on("input", "input", e => {
-            dialog.updateOkButton(true)
-        })
-
+    // get config
+    public async getConfig() {
+        const defaultBaseUrl = "https://graph.dcdc.io/extensions/VSO_BP_Extension"
+        const defaultconfig = {
+            baseUrl: defaultBaseUrl,
+            repositoryId: null,
+            repositoryPath: null,
+            repositoryType: null
+        }
+        const service = await VSS.getService<ExtensionDataService>(VSS.ServiceIds.ExtensionData)
+        const savedconfig = await service.getValue("global.config")
+        return { ...defaultconfig, ...savedconfig }
     }
 
-    return null
+    public async run() {
+        let config = await this.getConfig()
+        const projectId = VSS.getWebContext().project.id
+        const projectName = VSS.getWebContext().project.name
+        const gitclient = GitHttpClient.getClient()
+        const tfclient = TfvcRestClient.getClient()
+        const gitRepos = await gitclient.getRepositories(projectId)
+
+        if (config.repositoryType == null) {
+
+            const validate = () => (repType.value === "TFS"
+                || (repType.value === "git" && gitRepos.some(x => x.name === gitSelect.value)))
+
+            const dlg = $("<div />")
+            dlg.append(`<h3>Project: ${projectName}`)
+            dlg.append(`<p>Where are your business process models stored?</p>`)
+
+            const gitSelect = {
+                enabled: gitRepos.length > 0,
+                mode: "drop",
+                source: gitRepos.map(r => r.name),
+                width: "400px"
+            } as Combos.IComboOptions
+
+            const repType = {
+                mode: "drop",
+                source: [
+                    "TFS",
+                    "git"
+                ],
+                value: gitRepos.length > 0 ? "git" : "TFS",
+                width: "400px",
+                change() {
+                    gitSelectCtrl.setEnabled(this.getText() === "git")
+                }
+            } as Combos.IComboOptions
+
+            $("<label />").text("Repository Type:").appendTo(dlg);
+            const repTypeCtrl = Controls.create(Combos.Combo, dlg, repType)
+            $("<label />").text("Git Repository:").appendTo(dlg);
+            const gitSelectCtrl = Controls.create(Combos.Combo, dlg, gitSelect)
+
+            const dialog = Dialogs.show(Dialogs.ModalDialog, {
+                content: dlg,
+                title: "Configure",
+                async okCallback(result: any) {
+                    config = {...config, ...result}
+                }
+            } as Dialogs.IModalDialogOptions)
+            const ele = dialog.getElement()
+            ele.on("input", "input", e => {
+                dialog.setDialogResult({
+                    repositoryId: gitRepos.find(x => x.name === gitSelect.value).id,
+                    repositoryType: repType.value
+                })
+                dialog.updateOkButton(validate())
+            })
+
+        }
+
+        return null
+    }
 }
 
-main()
+const bp = new BusinessProcess()
+bp.run()
